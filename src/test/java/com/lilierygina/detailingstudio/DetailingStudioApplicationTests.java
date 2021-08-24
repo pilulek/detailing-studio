addc4bac9529f347b861af01e571597adb3ac6e0
pilulek <pilulek777@gmail.com>
2021-08-24 13:02:53 +0300

    Add spring boot project

---
+++ src/test/java/com/lilierygina/detailingstudio/DetailingStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilierygina.detailingstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetailingStudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
