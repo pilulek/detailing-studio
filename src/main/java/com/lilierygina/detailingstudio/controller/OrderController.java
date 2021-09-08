18b14144f84849ea8f89594a824b9b45a46e179f
pilulek <60519890+pilulek@users.noreply.github.com>
2021-09-08 13:03:57 +0300

    Br 6 add controller (#8)
    * Add controller
    
    * Update OrderServiceImpl.java
    
    * Update OrderController.java

--- src/main/java/com/lilierygina/detailingstudio/controller/OrderController.java
+++ src/main/java/com/lilierygina/detailingstudio/controller/OrderController.java
@@ -1,7 +1,12 @@
 package com.lilierygina.detailingstudio.controller;
 
+import com.lilierygina.detailingstudio.entity.Order;
+import com.lilierygina.detailingstudio.service.OrderService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/orders")
@@ -11,5 +16,55 @@ public class OrderController {
   // constructor injection
   public OrderController(OrderService theOrderService) {
     orderService = theOrderService;
+  }
+
+  // add mapping for "/list"
+  @GetMapping("/list")
+  public String listOrders(Model theModel) {
+    //get orders from db
+    List<Order> theOrders = orderService.findAll();
+
+    // add to the spring model (our Thymeleaf template will access this data
+    theModel.addAttribute("orders", theOrders);
+    return "orders/all-order-list";
+  }
+
+  @GetMapping("/showFormForAdd")
+  public String showFormForAdd(Model theModel) {
+    // create model attribute to bind form data
+    Order theOrder = new Order();
+
+    // thymeleaf template will access this data for binding form data
+    theModel.addAttribute("order", theOrder);
+
+    return "orders/order-form";
+  }
+
+  @GetMapping("/showFormForUpdate")
+  public String showFormForUpdate(@RequestParam("orderId") int theId, Model theModel) {
+    // get the order from the service
+    Order theOrder = orderService.findById(theId);
+
+    // set order as a model attribute to pre-populate the form
+    theModel.addAttribute("order", theOrder);
+    // send over to form
+    return "orders/order-form";
+  }
+
+  @PostMapping("/save")
+  public String saveOrder(@ModelAttribute("order") Order theOrder) {
+    // save the order
+    orderService.save(theOrder);
+    // use a redirect to prevent duplicate submissions
+    return "redirect:/orders/list";
+  }
+
+  @GetMapping("/delete")
+  public String deleteOrder(@RequestParam("orderId") int theId) {
+    // delete the order
+    orderService.deleteById(theId);
+
+    // redirect to /orders/list
+    return "redirect:/orders/list";
   }
 }
--- src/main/java/com/lilierygina/detailingstudio/service/OrderServiceImpl.java
+++ src/main/java/com/lilierygina/detailingstudio/service/OrderServiceImpl.java
@@ -34,7 +34,7 @@ public class OrderServiceImpl implements OrderService {
       theOrder = result.get();
     } else {
       // if we didn't find the order
-      throw new RuntimeException("Did not find employee id - " + theId);
+      throw new RuntimeException("Did not find order id - " + theId);
     }
     return theOrder;
   }
