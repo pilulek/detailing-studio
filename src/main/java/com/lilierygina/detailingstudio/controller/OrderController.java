86ef79d786d4ba79bdc8a3c45240a5e756450255
pilulek <60519890+pilulek@users.noreply.github.com>
2021-09-08 12:14:45 +0300

    Create OrderController.java (#7)

---
+++ src/main/java/com/lilierygina/detailingstudio/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.lilierygina.detailingstudio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+   private OrderService orderService;
+
+  // constructor injection
+  public OrderController(OrderService theOrderService) {
+    orderService = theOrderService;
+  }
+}
