4838f61f60f39a287df52965686e8bc3620291b9
pilulek <pilulek777@gmail.com>
2021-09-08 12:07:03 +0300

    Rename

--- src/main/java/com/lilierygina/detailingstudio/dao/EmoloyeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lilierygina.detailingstudio.dao;
-
-import com.lilierygina.detailingstudio.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmoloyeeRepository extends JpaRepository<Order, Integer> {
-}
